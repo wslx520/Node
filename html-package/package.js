920f86c29d23749c4e22718dcf7db7c00b132e3f
李兴 <lixing@jingoal.com>
2016-04-25 11:57:20 +0800

    可以用了
    在append几个文件的内容到指定文件中时，需要注意保持先后顺序不出错，所以不能无脑append

--- html-package/package.js
+++ html-package/package.js
@@ -1,20 +1,64 @@
 'use strict';
 const fs = require('fs');
-
+const path = require('path');
+function read_write (from, to, callback) {
+    console.log(from, to)
+    // fs.createReadStream(from).pipe(fs.createWriteStream(to));
+    fs.createReadStream(from).on('data', function (chunk) {
+        fs.appendFile(to, chunk, function (err) {
+            console.log(err);
+            if(typeof callback === 'function') {
+                callback();
+            }
+        })
+    })
+}
 function main () {
-    var target = fs.createWriteStream('./target.html');
-    var rs = fs.createReadStream('./index.html', {
+    let targetpath = './target.html';
+    let target = fs.createWriteStream(targetpath);
+    let rs = fs.createReadStream('./index.html', {
       flags: 'r',
       encoding: 'utf-8',
       fd: null,
       mode: '0666',
       bufferSize: 10 * 1024
     });
+    let reg = /\<\?([\s\S]+?)\?\>/g;
+    let ma;
+    fs.writeFile(targetpath,'', function (err) {
+        if(err) {
+            console.log(err, '清空文件失败')
+        }
+    })
     rs.on('data', function (chunk) {
-        let reg = /\<\?[\s\S]+?\?\>/g;
-        let res = chunk.split(reg);
-        console.log(chunk.match(reg))
-        console.log(res, res.length)
+        let index = 0;
+
+        function match_append () {        
+            ma = reg.exec(chunk);
+            if(ma) {
+                let curIndex = ma.index;
+                fs.appendFile(targetpath, chunk.substring(index, curIndex), function (err) {
+                    console.log(err);
+                });
+                console.log(curIndex,index)
+                index = curIndex + ma[0].length;
+                let part = ma[1].trim().split(/\s+/);
+                let partname = part[1].replace(/\'|\"|\;/g, '');
+                console.log(partname);
+                let file = partname+'.html';
+                // file = path.normalize(file);
+                read_write(file, targetpath, match_append);
+            } else {
+                reg.lastIndex = 0;
+            }
+        }
+        match_append();
+        // let res = chunk.split(reg);
+        // console.log(chunk.match(reg))
+        // console.log(res, res.length)
+    })
+    rs.on('end', function () {
+        console.log('file end');
     })
 }
 main();
