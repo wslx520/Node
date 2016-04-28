eb776d31d994c4f578db743ec23354484fcdb4c1
李兴 <lixing@jingoal.com>
2016-04-28 18:02:41 +0800

    更新从数组中删除多个元素的方法测试
    splice依然落后

--- Matcha/test1.js
+++ Matcha/test1.js
@@ -8,13 +8,21 @@ suite('Remove one element from an array', function () {
     list.pop()
   }
 
+  function spliceMore (list, index, num) {
+    num = num || 1;
+    for (var i = index, k = i + num, n = list.length; k < n; i += 1, k += 1) {
+      list[i] = list[k]
+    }
+    list.length = list.length - num
+  }
+  
   bench('Array#splice', function () {
-    let array = [1, 2, 3]
-    array.splice(1, 1)
+    let array = [1, 2, 3,1, 2, 3,1, 2, 3]
+    array.splice(1, 3)
   })
 
   bench('EventEmitter way', function () {
-    let array = [1, 2, 3]
-    spliceOne(array, 1)
+    let array = [1, 2, 3,1, 2, 3,1, 2, 3]
+    spliceMore(array, 1, 3)
   })
 })
