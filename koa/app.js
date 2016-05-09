5e1b5d6a78b890d77a612394aa17ef1e8e4498c7
李兴 <lixing@jingoal.com>
2016-05-09 11:29:42 +0800

    koa与etpl-wrap配合

--- koa/app.js
+++ koa/app.js
@@ -1,16 +1,13 @@
 const Koa = require('koa');
-const etpl = require('etpl');
-const views = require('koa-views');
+const etplWrap = require('etpl-wrap');
 const app = new Koa();
 
 // 此处开始堆叠各种中间件//...
-app.use(views(__dirname + '/views', 'html', {
-    map:'etpl'
-}))
-
+var etpl = new etplWrap('./views','.html');
 app.use(ctx => {  
     // ctx.body = 'Hello Koa';
-    ctx.body = etpl.compile('Hello ${koa}')({koa:'this is Koa2!'})
+    // ctx.body = etpl.compile('Hello ${koa}')({koa:'this is Koa2!'})
+    ctx.body = etpl.render('index', {welcome:'koa与etpl-wrap测试', koa:'this is Koa2!', 'engine': 'etpl and etpl-wrap'});
 });
 
 app.listen(3000);
